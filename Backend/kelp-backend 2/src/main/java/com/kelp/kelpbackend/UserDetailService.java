ebf109044baf959433ac021c4a825cc77ac2e361
Sage Kolpin <sagekolpin@Sages-MacBook-Air.local>
2022-07-05 20:56:00 -0700

    oh no no

---
+++ Backend/kelp-backend 2/src/main/java/com/kelp/kelpbackend/UserDetailService.java
@@ -0,0 +1,7 @@
+// package main.java.com.kelp.kelpbackend;
+
+
+// public interface UserDetailService {
+//     UserDetails loadUserByUsername(String username)
+//     throws UsernameNotFoundExecption;
+// }
